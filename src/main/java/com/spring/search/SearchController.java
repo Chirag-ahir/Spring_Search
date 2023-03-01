d4d60e49e0568a6e4c28e8ed552c1a0e2be3f8c7
chirag ahir <ahirchirag1459@gmail.com>
2023-03-01 14:02:39 -0500

    dd

--- src/main/java/com/spring/search/SearchController.java
+++ src/main/java/com/spring/search/SearchController.java
@@ -17,6 +17,7 @@ public class SearchController {
 	public RedirectView search(@RequestParam("querybox") String query) {
 		
 //		System.out.print(false)
+//		System.out.print(false)
 		String url = "https://www.bing.com?q=" + query;
 		
 		RedirectView redirectView = new RedirectView();
